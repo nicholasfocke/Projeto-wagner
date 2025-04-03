a26397287fb93b0b95069e9922c687123df088c5
nicholasfocke <nicholasfocke05@gmail.com>
2025-04-03 11:26:46 -0300

    Update def exibir menu

---
+++ main.py
@@ -0,0 +1,9 @@
+def menu():
+    print('--Bem vindo ao sistema de extravio de bagagem--')
+    print('--Clique na opção que deseja abaixo--')
+    print("1 - Cadastrar Usuário")
+    print("2 - Editar Usuário")
+    print("3 - Excluir Usuário")
+    print("4 - Exibir Usuários")
+    print("0 - Sair")
+
--- testepedro.py
+++
@@ -1,2 +0,0 @@
-print('e ai irmao blz?(digita outro print aqui em baixo sem apagar esse print daqui)')
-print("Sport Recife maior do nordeste")
